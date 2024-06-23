fb644735291dae91ade2b662a858a4c9a452188c
Sandeep <sandeepdyadav0905@gmail.com>
2024-06-23 22:38:43 +0530

    Added Partitions

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React from 'react';
+import NavBar from '../components/NavBar';
 
 const Home = () => {
   return (
     <div>
-        <NavBar />
+      <NavBar />
+      <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
+        <div style={boxStyle}></div>
+        <div style={boxStyle}></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+const boxStyle = {
+  flex: 1,
+  height: '800px',
+  borderRadius: '10px',
+  backgroundColor: '#f0f0f0',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  margin: '10px',
+};
+
+export default Home;
