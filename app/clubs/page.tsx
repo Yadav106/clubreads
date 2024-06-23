410c3e096b07fd8c3d21d34f594cc0ed8fba8628
Yadav106 <sahil.jp.yadav@gmail.com>
2024-06-24 00:16:50 +0530

    made changes on clubs page

--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
+import NavBar from '../components/NavBar'
 
 const Clubs = () => {
     // const session = useSession()
     // session.data.user?.email
   return (
-    <div>Clubs</div>
+    <div className='h-[100%]'>
+      <NavBar />
+    </div>
   )
 }
 
