a7bbcf01f49c1c106d4addaaf532ae300b2bcc3a
Sandeep <sandeepdyadav0905@gmail.com>
2024-06-24 05:28:21 +0530

    Added Logo and Added css for Navbar

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <Image alt="logo" src="next.svg" width={100} height={100} className="mx-auto"/>
+            <Image alt="logo" src="/logo2.jpg" width={350} height={350} className="mx-auto rounded-full"/>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                 Sign In to your account
             </h2>
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,41 +1,53 @@
 "use client"
 
-import React from 'react'
-import UserImage from './UserImage'
-import { useRouter } from 'next/navigation'
+import React from 'react';
+import UserImage from './UserImage';
+import { useRouter } from 'next/navigation';
 
 const NavBar = () => {
-    const router = useRouter()
-  return (
-    <div className='h-[50px] bg-[#f3ece4] mt-[1rem] w-[80vw] mx-[10vw] rounded-[12px] px-5 flex justify-between items-center shadow-sm shadow-black/40'>
-        <div className='flex gap-5'>
-            <div className='hover:font-bold cursor-pointer' onClick={() => router.push('/home')}>
-                Home
-            </div>
-            <div className='hover:font-bold cursor-pointer' onClick={() => router.push('/clubs')}>
-                Clubs
-            </div>
-        </div>
-
-        <div className='flex gap-5'>
-            <div className="flex items-center space-x-4">
-                <div className="relative">
-                    <input 
-                        type="text" 
-                        placeholder="Search clubs" 
-                        className="px-6 py-1 rounded-md border border-gray-300"
-                    />
-                    <img 
-                        src="/search.svg" 
-                        alt="Search Icon" 
-                        className="absolute right-3 top-2 w-5 h-5 text-gray-400"
-                    />
+    const router = useRouter();
+    return (
+        <div className='h-[50px] bg-[#f3ece4] mt-[1rem] w-[80vw] mx-[10vw] rounded-[12px] px-5 flex justify-between items-center shadow-sm shadow-black/40'>
+            <div className='flex items-center gap-2'>
+                <img 
+                    src='/logo1.jpg' 
+                    alt='Logo' 
+                    className='h-[28px] cursor-pointer' 
+                    onClick={() => router.push('/home')}
+                />
+                <div 
+                    className='nav-item cursor-pointer px-4 py-2 rounded'
+                    onClick={() => router.push('/home')}
+                >
+                    Home
+                </div>
+                <div 
+                    className='nav-item cursor-pointer px-4 py-2 rounded'
+                    onClick={() => router.push('/clubs')}
+                >
+                    Clubs
                 </div>
             </div>
-            <UserImage />
+
+            <div className='flex gap-5'>
+                <div className="flex items-center space-x-4">
+                    <div className="relative">
+                        <input 
+                            type="text" 
+                            placeholder="Search clubs" 
+                            className="px-6 py-1 rounded-md border border-gray-300"
+                        />
+                        <img 
+                            src="/search.svg" 
+                            alt="Search Icon" 
+                            className="absolute right-3 top-2 w-5 h-5 text-gray-400"
+                        />
+                    </div>
+                </div>
+                <UserImage />
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default NavBar
+export default NavBar;
