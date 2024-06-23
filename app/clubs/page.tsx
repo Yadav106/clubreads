044f4b550b867a9591a9db54fed7797feb0356b1
Sandeep <sandeepdyadav0905@gmail.com>
2024-06-23 22:01:43 +0530

    Added Clubs Page & modified navbar

---
+++ app/clubs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSession } from 'next-auth/react'
+
+const Clubs = () => {
+    // const session = useSession()
+    // session.data.user?.email
+  return (
+    <div>Clubs</div>
+  )
+}
+
+export default Clubs
